1b022e58a4de28c11dce924a20ae29f190b79e79
MerattiGiacomo <72219680+MerattiGiacomo@users.noreply.github.com>
2022-10-12 12:58:10 +0200

    Add files via upload

---
+++ Progetto1 v3/socket-server/src/main/java/it/fi/itismeucci/myThread.java
@@ -0,0 +1,34 @@
+package it.fi.itismeucci;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class myThread extends Thread{
+    Socket client;
+    public myThread(Socket client){
+        this.client = client;
+    }
+
+    public void run() {
+        try {
+            communicate(client);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Errore di server");
+        }
+    }
+
+    void communicate(Socket client) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+        String recv = in.readLine();
+        System.out.print("Stringa ricevuta: " + recv);
+        String modifiedRecv = recv.toUpperCase();
+        out.writeBytes(modifiedRecv + '\n');
+        client.close();
+
+    }
+}
